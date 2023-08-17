6c34a552f8cddecc434d71fcdd17780b6eaebde6
Angad Singh <gatecrash360@gmail.com>
2023-08-18 00:03:12 +0530

    PRM-004: Refactoring
    - improved UI for login, homepage and register

--- gui/gui.py
+++ gui/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.ttk import Style, Button
 
 from gui import gui_logic
 
@@ -6,16 +7,46 @@ from gui import gui_logic
 class MainScreen(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Main Screen")
-        self.geometry("640x480+200+200")
-        self.configure(background='grey')
+
+        # Set window properties
         self.title("PyRestoManager")
+        self.configure(background='gray')
 
-        tk.Label(text="PyRestoManager", fg="white", bg="grey", width="900", height="2",
-              font=("Calibri", 14)).pack()
+        # Calculate screen dimensions
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = (screen_width - 640) // 2
+        y = (screen_height - 480) // 2
+        self.geometry(f"640x480+{x}+{y}")
 
-        tk.Button(self, text="Login", command=self.show_login).pack()
-        tk.Button(self, text="Register", command=self.show_register).pack()
+        # Create title label
+        title_label = tk.Label(self, text="Welcome to PyRestoManager", fg="white", bg="gray",
+                               font=("Calibri", 18))
+        title_label.pack(pady=20)
+
+        # Create buttons with styling
+        button_style = Style()
+        button_style.configure("MainButton.TButton", font=("Calibri", 12))
+
+        burger_label = tk.Label(self, text="🍔", font=("Arial", 72), bg="gray")
+        burger_label.pack(pady=20)
+
+        login_button = Button(self, text="Login",style="MainButton.TButton", command=self.show_login)
+        login_button.pack(pady=10)
+
+        register_button = Button(self, text="Register",style="MainButton.TButton",   command=self.show_register)
+        register_button.pack(pady=10)
+
+        # Create display message label
+        display_message = tk.Label(self, text="Elevate Your Restaurant Business!", fg="white", bg="grey",
+                                   font=("Calibri", 15))
+        display_message.pack(pady=20)
+
+    def show_login(self):
+        print("Login button clicked")
+
+    def show_register(self):
+        print("Register button clicked")
 
     def show_register(self):
         gui_logic.show_register_screen(self)
--- gui/gui_logic.py
+++ gui/gui_logic.py
@@ -11,23 +11,38 @@ def show_register_screen(main_screen):
     register_screen = tk.Toplevel(main_screen)
     register_screen.title("Register User")
     register_screen.geometry("640x480+200+200")
+    register_screen.configure(background='lightgray')  # Light gray background
 
     username = tk.StringVar()
     password = tk.StringVar()
 
-    tk.Label(register_screen, text="Please enter details below").pack()
-    tk.Label(register_screen, text="").pack()
-    tk.Label(register_screen, text="Username * ").pack()
+    tk.Label(register_screen, text="Create an Account", fg="black", bg="lightgray",
+             font=("Calibri", 18, "bold")).pack(pady=10)
 
-    username_entry = tk.Entry(register_screen, textvariable=username)
-    username_entry.pack()
-    tk.Label(register_screen, text="Password * ").pack()
-    password_entry = tk.Entry(register_screen, show="*", textvariable=password)
-    password_entry.pack()
-    tk.Label(register_screen, text="").pack()
-    tk.Label(register_screen, text="Register User").pack()
-    tk.Button(register_screen, text="Register", width=10, height=1,
-              command=lambda: register_run_logic(username, password, register_screen, main_screen)).pack()
+    frame = tk.Frame(register_screen, background='lightgray')
+    frame.pack(pady=20, padx=20, fill='both', expand=True)
+
+    tk.Label(frame, text="Username:", fg="black",bg="lightgray", font=("Calibri", 14)).pack(anchor='w')
+    username_entry = tk.Entry(frame, textvariable=username, font=("Calibri", 14))
+    username_entry.pack(fill='x', pady=5)
+
+    tk.Label(frame, text="Password:", fg="black",bg="lightgray", font=("Calibri", 14)).pack(anchor='w')
+    password_entry = tk.Entry(frame, show="*", textvariable=password, font=("Calibri", 14))
+    password_entry.pack(fill='x', pady=5)
+
+    register_button = tk.Button(frame, text="Register", width=15, height=1,
+                                command=lambda: register_run_logic(username, password, register_screen, main_screen),
+                                font=("Calibri", 14), bg="green", fg="white")
+    register_button.pack(pady=10)
+
+    tk.Label(register_screen, text="Already have an account?", bg="lightgray",font=("Calibri", 12)).pack(pady=10)
+    login_link = tk.Label(register_screen, text="Login here", bg="lightgray",fg="blue", cursor="hand2", font=("Calibri", 12))
+    login_link.pack()
+    login_link.bind("<Button-1>", lambda event: show_login_screen(register_screen))
+
+    tk.Label(register_screen, text="Explore PyRestoManager and elevate your restaurant business!",bg="lightgray", fg="black",
+             font=("Calibri", 15, "italic")).pack(pady=20)
+
 
 
 def register_run_logic(username, password, register_screen, main_screen):
@@ -50,21 +65,33 @@ def show_login_screen(main_screen):
     main_screen.withdraw()  # Hide the main screen
     login_screen = tk.Toplevel(main_screen)
     login_screen.title("Login Screen Open Now")
-    login_screen.geometry("640x480+200+200")
 
-    tk.Label(login_screen, text="Please enter details below to login").pack()
-    tk.Label(login_screen, text="").pack()
+    # Calculate screen dimensions
+    screen_width = login_screen.winfo_screenwidth()
+    screen_height = login_screen.winfo_screenheight()
+    x = (screen_width - 640) // 2
+    y = (screen_height - 480) // 2
+    login_screen.geometry(f"640x480+{x}+{y}")
+
+    login_screen.configure(background='gray')
+
+    tk.Label(login_screen, text="Please enter details below to login", fg="white", bg="gray",
+             font=("Calibri", 18)).pack(pady=20)
 
     username_verify = tk.StringVar()
     password_verify = tk.StringVar()
 
-    username_entry1 = tk.Entry(login_screen, textvariable=username_verify)
-    username_entry1.pack()
-    tk.Label(login_screen, text="").pack()
-    password_entry1 = tk.Entry(login_screen, show="*", textvariable=password_verify)
-    password_entry1.pack()
-    tk.Label(login_screen, text="").pack()
-    tk.Button(login_screen, text="Login Button", command=lambda: login_run_logic(username_verify, password_verify, username_entry1, password_entry1, login_screen, main_screen)).pack()
+    tk.Label(login_screen, text="Username:", fg="white", bg="gray", font=("Calibri", 14)).pack()
+    username_entry1 = tk.Entry(login_screen, textvariable=username_verify, font=("Calibri", 14))
+    username_entry1.pack(pady=10)
+
+    tk.Label(login_screen, text="Password:", fg="white", bg="gray", font=("Calibri", 14)).pack()
+    password_entry1 = tk.Entry(login_screen, show="*", textvariable=password_verify, font=("Calibri", 14))
+    password_entry1.pack(pady=10)
+
+    login_button = tk.Button(login_screen, text="Login", command=lambda: login_run_logic(username_verify, password_verify, username_entry1, password_entry1, login_screen, main_screen), font=("Calibri", 14), bg="green", fg="white")
+    login_button.pack(pady=20)
+
 
 
 def login_run_logic(username, password, userentry, passentry, login_screen, main_screen):
@@ -84,16 +111,41 @@ def login_success(register_screen, main_screen):
     register_screen.destroy()
     main_screen.deiconify()  # Show the main screen again
 
-
-def show_home_screen(username1,login_screen2, main_screen):
+def show_home_screen(username1, login_screen2, main_screen):
     login_screen2.destroy()
     homeScreen = tk.Toplevel(main_screen)
     homeScreen.title("Home Screen Open Now")
-    homeScreen.geometry("640x480+200+200")
 
+    # Calculate screen dimensions
+    screen_width = homeScreen.winfo_screenwidth()
+    screen_height = homeScreen.winfo_screenheight()
+    x = (screen_width - 640) // 2
+    y = (screen_height - 480) // 2
+    homeScreen.geometry(f"640x480+{x}+{y}")
 
-    tk.Label(homeScreen, text="Welcome " + username1).pack()
-    tk.Label(homeScreen, text="").pack()
-    tk.Button(homeScreen, text="Menu Editor", command=lambda:showMenuEditorUI(homeScreen, username1)).pack()
-    tk.Label(homeScreen, text="").pack()
-    tk.Button(homeScreen, text="Employee Editor", command=lambda:employee_editor_open(homeScreen, username1)).pack()
+    homeScreen.configure(background='lightgray')  # Light gray background
+
+    welcome_label = tk.Label(homeScreen, text="Welcome, " + username1 + "!", fg="black",
+                             font=("Calibri", 18, "italic"))
+    welcome_label.pack(pady=20)
+
+    options_frame = tk.Frame(homeScreen, background='lightgray', padx=20, pady=20, bd=2, relief="groove")
+    options_frame.pack()
+
+    menu_editor_button = tk.Button(options_frame, text="Menu Editor", command=lambda: showMenuEditorUI(homeScreen, username1),
+                                   font=("Calibri", 14), bg="green", fg="white")
+    menu_editor_button.grid(row=0, column=0, padx=20, pady=10)
+    tk.Label(options_frame, text="Create Delicious Dishes", font=("Calibri", 12, "italic"), fg="black",
+             background='lightgray').grid(row=1, column=0)
+    tk.Label(options_frame, text="🍔", font=("Arial", 24), bg='lightgray').grid(row=2, column=0)
+
+    # Divider Label
+    divider_label = tk.Label(options_frame, text="", background="gray", height=1, relief="sunken")
+    divider_label.grid(row=0, column=1, rowspan=3, padx=10, sticky='ns')
+
+    employee_editor_button = tk.Button(options_frame, text="Employee Editor", command=lambda: employee_editor_open(homeScreen, username1),
+                                       font=("Calibri", 14), bg="blue", fg="white")
+    employee_editor_button.grid(row=0, column=2, padx=20, pady=10)
+    tk.Label(options_frame, text="Manage Your Superstar Staff", font=("Calibri", 12, "italic"), fg="black",
+             background='lightgray').grid(row=1, column=2)
+    tk.Label(options_frame, text="👨‍🍳", font=("Arial", 24), bg='lightgray').grid(row=2, column=2)
